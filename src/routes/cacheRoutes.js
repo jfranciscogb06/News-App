542ec420343dc484d113c1a4c25bbd84da73b06a
jfranciscogb06 <jfranciscogb06@gmail.com>
2025-03-11 14:58:53 -0400

    get endpoint

--- src/routes/cacheRoutes.js
+++ src/routes/cacheRoutes.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 const stockController = require('../controllers/stockController');
 
-// Route for clearing the cache
+// Routes for clearing the cache - support both GET and POST
+router.get('/clear', stockController.clearCache.bind(stockController));
 router.post('/clear', stockController.clearCache.bind(stockController));
 
 module.exports = router; 
