b06219bab87695f57c425ad1b97718fe91bbb1b2
jfranciscogb06 <jfranciscogb06@gmail.com>
2025-03-31 19:51:35 -0400

    moved api key

--- src/config/config.js
+++ src/config/config.js
@@ -2,20 +2,20 @@ require('dotenv').config();
 
 module.exports = {
   port: process.env.PORT || 3000,
+  database: {
+    url: process.env.DATABASE_URL
+  },
   openai: {
     apiKey: process.env.OPENAI_API_KEY
   },
-  newsapi: {
-    apiKey: process.env.NEWSAPI_API_KEY
-  },
   google: {
     apiKey: process.env.GOOGLE_API_KEY,
-    searchEngineId: process.env.GOOGLE_SEARCH_ENGINE_ID || '' // Optional for now
+    searchEngineId: process.env.GOOGLE_SEARCH_ENGINE_ID
   },
   serpapi: {
-    apiKey: process.env.SERPAPI_API_KEY || '4eec45a26530daedf7c8bdd7c46c114550845dc197447bd880a507ec334103bd'
+    apiKey: process.env.SERPAPI_API_KEY
   },
-  database: {
-    url: process.env.DATABASE_URL
+  newsapi: {
+    apiKey: process.env.NEWSAPI_API_KEY
   }
 }; 
